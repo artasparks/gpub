8a0e1275d3c84864d1a937f8bdb13142bb794688
kashomon <jrhoak@gmail.com>
2014-10-18 10:15:49 -0600

    Add skeleton collection gen wrapper

---
+++ src/scripts/diagrams/collection_gen.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var glift = require('../glift.js');
+var gpub = require('../gpub.js');
+var flagz = require('../flagz.js')
+
+var fs = require('fs');
+var path = require('path')
