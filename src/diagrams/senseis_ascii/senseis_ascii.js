2c7f6724cb4dff7e3bc087bfd9c1c187be36956f
kashomon <jrhoak@gmail.com>
2015-02-23 22:59:22 -0700

    Added example to sensei's ascii.

--- src/diagrams/senseis_ascii/senseis_ascii.js
+++ src/diagrams/senseis_ascii/senseis_ascii.js
@@ -1,3 +1,20 @@
+/**
+ * ASCII in the Sensei's library format.  See: 
+ * http://senseis.xmp.net/?HowDiagramsWork
+ *
+ * Example:
+ * $$ A [joseki] variation
+ * $$  ------------------
+ * $$ | . . . . . . . . .
+ * $$ | . . . . . . . . .
+ * $$ | . . 7 3 X d . . .
+ * $$ | . . O 1 O 6 . . .
+ * $$ | . . 4 2 5 c . . .
+ * $$ | . . 8 X a . . . .
+ * $$ | . . . b . . . . .
+ * $$ | . . . . . . . . .
+ * $$ | . . . . . . . . .
+ */
 gpub.diagrams.senseisAscii = {
   create: function(flattened, opts) {
     var toStr = glift.flattener.symbolStr;
