9e710198af93bde0aa581e1c59da6486c4c42a57
kashomon <jrhoak@gmail.com>
2015-03-22 07:37:54 -0600

    fix indentation on comment.

--- src/book/latex/sanitize.js
+++ src/book/latex/sanitize.js
@@ -1,8 +1,8 @@
-  /**
-   * Sanitizes latex input. This isn't particularly robust, but it is meant to
-   * protect us against accidental problematic characters rather than malicious
-   * user input.
-   */
+/**
+ * Sanitizes latex input. This isn't particularly robust, but it is meant to
+ * protect us against accidental problematic characters rather than malicious
+ * user input.
+ */
 gpub.book.latex.sanitize = function(text) {
   return text.replace(/[$#}{]/g, function(match) {
     return '\\' + match;
