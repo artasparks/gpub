1dd2e7df2a9682297744c690012caf01add31a4a
kashomon <jrhoak@gmail.com>
2015-03-22 20:24:37 -0700

    Rm old markdown lib.

--- src/markdown/markdown.js
+++ src/markdown/markdown.js
@@ -1,6 +1,7 @@
 /**
- * Markdown for Gpub.
+ * Markdown for Gpub. Note that Glift comes pre-packaged with markdown.
+ *
+ * Most output formats will want to defined a custom renderer.
  */
 gpub.markdown = {
-  // TODO(kashomon): Write a 'marked' render.
 };
