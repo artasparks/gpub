0987ddc88b64c9893797f2f0a823fd018880e079
kashomon <jrhoak@gmail.com>
2015-09-24 09:18:05 -0600

    Use 'for' instead of 'instead of'

--- src/book/latex/context.js
+++ src/book/latex/context.js
@@ -141,7 +141,7 @@ gpub.book.latex.context = {
         if (ref) {
           baseLabel += '\\hyperref[' + ref + ']{'
         }
-        baseLabel += '\\textit{instead of} '
+        baseLabel += '\\textit{for} '
         baseLabel += readableColor + ' ' + moveNum;
         if (ref) {
           baseLabel += '}';
