ed2827ea94d9b05d5a8a312aebc77a32f647644d
Josh Hoak <jrhoak@gmail.com>
2016-11-03 06:27:57 -0700

    Update gpub with new glift svg awesomeness.

---
+++ deps/glift-core/svg/svgobj_test.js
@@ -0,0 +1,39 @@
+(function() {
+  module('glift.svg.svgObjTest');
+  var svg = glift.svg;
+
+  test('Test svg creation', function() {
+    deepEqual(svg.svg().render(),
+        '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>');
+  });
+
+  test('Test circle creation', function() {
+    deepEqual(svg.circle().render(), '<circle></circle>');
+  });
+
+  test('Test rect creation', function() {
+    deepEqual(svg.rect().render(), '<rect></rect>');
+  });
+
+  test('Test path creation', function() {
+    deepEqual(svg.path().render(), '<path></path>');
+  });
+
+  test('Test image creation', function() {
+    deepEqual(svg.image().render(), '<image></image>');
+  });
+
+  test('Test circle+attr creation', function() {
+    var circle = svg.circle().setAttr('r', 30).render();
+    deepEqual(circle, '<circle r="30"></circle>');
+  });
+
+  test('Test circle creation, with optional attrs', function() {
+    deepEqual(svg.circle({'r': 30}).render(), '<circle r="30"></circle>');
+  });
+
+  test('Test group creation, with children', function() {
+    var group = svg.group().appendNew('circle', {'r': 30});
+    deepEqual(group.render(), '<g>\n<circle r="30"></circle>\n</g>');
+  });
+})();
