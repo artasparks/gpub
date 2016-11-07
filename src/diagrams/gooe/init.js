a92e6059619cd35d70c5b179fa692929a5eaf126
Josh Hoak <jrhoak@gmail.com>
2016-11-06 18:52:20 -0800

    update the gooe init to not use array joins.

--- src/diagrams/gooe/init.js
+++ src/diagrams/gooe/init.js
@@ -14,29 +14,23 @@ gpub.diagrams.gooe.init = {
    * fonts.
    */
   defs: {
-    sizeDefs: [
-      '% Size definitions',
-      '\\newdimen\\bigRaise',
-      '\\bigRaise=4.3pt',
-      '\\newdimen\\smallRaise',
-      '\\smallRaise=3.5pt',
-      '\\newdimen\\inlineRaise',
-      '\\inlineRaise=3.5pt'
-    ],
+    sizeDefs: '% Size definitions\n' +
+      '\\newdimen\\bigRaise\n' +
+      '\\bigRaise=4.3pt\n' +
+      '\\newdimen\\smallRaise\n' +
+      '\\smallRaise=3.5pt\n' +
+      '\\newdimen\\inlineRaise\n' +
+      '\\inlineRaise=3.5pt\n',
 
-    bigBoardDefs: [
-      '% Big-sized board defs',
-      '\\def\\eLblBig#1{\\leavevmode\\hbox to \\goIntWd{\\hss\\raise\\bigRaise\\hbox{\\tenpointeleven{#1}}\\hss}}',
-      '\\def\\goWsLblBig#1{\\leavevmode\\setbox0=\\hbox{\\0??!}\\rlap{\\0??!}\\raise\\bigRaise\\hbox to \\wd0{\\hss\\tenpointeleven{#1}\\hss}}',
-      '\\def\\goBsLblBig#1{\\leavevmode\\setbox0=\\hbox{\\0??@}\\rlap{\\0??@}\\raise\\bigRaise\\hbox to \\wd0{\\hss\\color{white}\\tenpointeleven{#1}\\color{white}\\hss}}'
-    ],
+    bigBoardDefs: '% Big-sized board defs' +
+      '\\def\\eLblBig#1{\\leavevmode\\hbox to \\goIntWd{\\hss\\raise\\bigRaise\\hbox{\\tenpointeleven{#1}}\\hss}}\n' +
+      '\\def\\goWsLblBig#1{\\leavevmode\\setbox0=\\hbox{\\0??!}\\rlap{\\0??!}\\raise\\bigRaise\\hbox to \\wd0{\\hss\\tenpointeleven{#1}\\hss}}\n' +
+      '\\def\\goBsLblBig#1{\\leavevmode\\setbox0=\\hbox{\\0??@}\\rlap{\\0??@}\\raise\\bigRaise\\hbox to \\wd0{\\hss\\color{white}\\tenpointeleven{#1}\\color{white}\\hss}}\n',
 
-    normalBoardDefs: [
-      '% Normal-sized board defs',
-      '\\def\\eLbl#1{\\leavevmode\\hbox to \\goIntWd{\\hss\\raise\\smallRaise\\hbox{\\tenpoint{#1}}\\hss}}',
-      '\\def\\goWsLbl#1{\\leavevmode\\setbox0=\\hbox{\\0??!}\\rlap{\\0??!}\\raise\\smallRaise\\hbox to \\wd0{\\hss\\eightpointnine{#1}\\hss}}',
-      '\\def\\goBsLbl#1{\\leavevmode\\setbox0=\\hbox{\\0??@}\\rlap{\\0??@}\\raise\\smallRaise\\hbox to \\wd0{\\hss\\color{white}\\eightpointnine{#1}\\color{white}\\hss}}'
-    ]
+    normalBoardDefs: '% Normal-sized board defs' +
+      '\\def\\eLbl#1{\\leavevmode\\hbox to \\goIntWd{\\hss\\raise\\smallRaise\\hbox{\\tenpoint{#1}}\\hss}}\n' +
+      '\\def\\goWsLbl#1{\\leavevmode\\setbox0=\\hbox{\\0??!}\\rlap{\\0??!}\\raise\\smallRaise\\hbox to \\wd0{\\hss\\eightpointnine{#1}\\hss}}\n' +
+      '\\def\\goBsLbl#1{\\leavevmode\\setbox0=\\hbox{\\0??@}\\rlap{\\0??@}\\raise\\smallRaise\\hbox to \\wd0{\\hss\\color{white}\\eightpointnine{#1}\\color{white}\\hss}}\n',
   },
 
   /**
@@ -44,20 +38,20 @@ gpub.diagrams.gooe.init = {
    *
    * Takes a base font family. Defaults to cmss (computer modern sans serif).
    * @param {string=} opt_baseFont
+   * @return {string} the latex init defs.
    */
   extraDefs: function(opt_baseFont) {
     var baseFont = opt_baseFont || 'cmss';
     var defs = gpub.diagrams.gooe.init.defs;
-    var fontDefsBase = [
-      '% Gooe font definitions',
-      '\\font\\tenpoint=' + baseFont + '10',
-      '\\font\\tenpointeleven=' + baseFont + '10 at 11pt',
-      '\\font\\eightpoint=' + baseFont + '8',
-      '\\font\\eightpointnine=' + baseFont + '8 at 9pt'
-    ]
-    return fontDefsBase
-      .concat(defs.sizeDefs)
-      .concat(defs.bigBoardDefs)
-      .concat(defs.normalBoardDefs).join('\n');
+    var fontDefsBase =
+      '% Gooe font definitions\n' +
+      '\\font\\tenpoint=' + baseFont + '10\n' +
+      '\\font\\tenpointeleven=' + baseFont + '10 at 11pt\n' +
+      '\\font\\eightpoint=' + baseFont + '8\n' +
+      '\\font\\eightpointnine=' + baseFont + '8 at 9pt\n';
+    return fontDefsBase +
+      defs.sizeDefs +
+      defs.bigBoardDefs +
+      defs.normalBoardDefs;
   }
 };
