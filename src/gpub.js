7fce4efb34b19770d5c42e088fc010af3ee6f068
Josh Hoak <jrhoak@gmail.com>
2016-12-01 08:04:14 -0800

    Bump node patch version

--- src/gpub.js
+++ src/gpub.js
@@ -3,7 +3,7 @@
  *
  * @copyright Josh Hoak
  * @license MIT License (see LICENSE.txt)
- * @version 0.3.5
+ * @version 0.3.6
  * --------------------------------------
  */
 (function(w) {
--- src/gpub_global.js
+++ src/gpub_global.js
@@ -4,5 +4,5 @@ gpub.global = {
    * See: http://semver.org/
    * Currently in alpha.
    */
-  version: '0.3.5',
+  version: '0.3.6',
 };
