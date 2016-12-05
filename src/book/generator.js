78635ec0d6bc9cd5fb9eabe7302945e3e02ee7d4
Josh Hoak <jrhoak@gmail.com>
2016-12-04 21:23:42 -0800

    Start to stub out the generator.

---
+++ src/book/generator.js
@@ -0,0 +1,27 @@
+gpub.provide('gpub.book.Generator');
+
+/**
+ * Book generator helper.
+ *
+ * @struct @final @constructor
+ */
+gpub.book.Generator = function() {
+
+};
+
+
+// Some stubbed methods
+gpub.book.Generator.prototype = {
+  /**
+   * Get the labels for an ID.
+   * @param 
+   */
+  getLabels: function(id) {
+  },
+
+  hasLabel: function(id) {
+  },
+
+  forEachDiagram: function(fn) {
+  }
+};
