a08cec674543da7197d0baad8d503a1a5435b56e
Josh Hoak <jrhoak@gmail.com>
2016-12-16 07:38:34 -0800

    Add a helper function for making smartgo points. And more comments.

--- gpub-node/gpub-concat.js
+++ gpub-node/gpub-concat.js
@@ -12215,29 +12215,63 @@ goog.provide('gpub.diagrams.smartgo');
 /**
  * Package for Smartgo-book rendering.
  */
-gpub.diagrams.smartgo = {};
+gpub.diagrams.smartgo = {
+  /**
+   * Converts a normal Glift point into Smartgo coordinate.
+   *
+   * Note: smart go diagrams are indexed from the bottom left:
+   * 19
+   * 18
+   * ..
+   * 3
+   * 2
+   * 1
+   *   A B C D E F G H J K ...
+   *
+   * As with Igo, I is skipped. Also, a strange property of SmartGo points is
+   * that they are concatenated together: A1M10O4. This is 3 points: A1,M10,O4.
+   *
+   * @param {!glift.Point} pt
+   * @param {number} size
+   * @return string
+   */
+  toSmartGoCoord: function(pt, size) {
+    if (!pt) { throw new Error('No point'); }
+    if (!size) { throw new Error('No board size'); }
+    if (pt.x() < 0 || pt.y() < 0 || pt.x() > size-1 || pt.y() > size-1) {
+      throw new Error('Pt out of bounds: ' + pt.toString());
+    }
+    var letters = 'ABCDEFGHJKLMNOPQRSTUVWXYZ';
+    var x = letters.charAt(pt.x());
+    var y = size - pt.y();
+    return x+y;
+  },
+};
 
 goog.provide('gpub.diagrams.smartgo.Renderer');
 
 /**
  * Spec: http://www.smartgo.com/pdf/gobookformat.pdf
  *
- *
  * Some notes:
  * -   All books must start with something like:
  *     ::book(#mahbook) title="Example Title" author="Somebody"
  * -   Each file contains one book -- the file extension should be .gobook.
  *
- * Note: smart go diagrams are indexed from the bottom left:
- * 19
- * 18
- * ..
- * 3
- * 2
- * 1
- *   A B C D E F G H J K ...
  *
- * As with Igo, I is skipped
+ * More details on figure/diagram types: from the doc:
+ *
+ * fig: Main figures of a game, based on a Go game given as Go data, with move
+ * numbers reflecting the moves in the game, and the move numbers listed below
+ * the figure. Shown at full size.
+ *
+ * dia: Diagrams showing alternative move sequences. Move numbering starts at
+ * 1.  Usually shown at a slightly smaller scale than the figures. (This
+ * default setting can be changed with the fullWidth attribute.)
+ *
+ * prb: Diagrams showing a problem diagram. Problems are shown at full width,
+ * and move input will react differently, giving feedback on correct or wrong
+ * solutions.
  *
  * @constructor @final @struct
  */
@@ -12254,9 +12288,8 @@ gpub.diagrams.smartgo.Renderer.prototype = {
   render: function(flat, opt) {
     return '';
   },
-
   /**
-   * Render-inline the
+   * Render-inline some inline text with smartgo
    * @param {string} text
    * @param {!gpub.api.DiagramOptions} opt
    * @return {string} The processed text
@@ -12265,6 +12298,7 @@ gpub.diagrams.smartgo.Renderer.prototype = {
     return '';
   }
 };
+
 
 goog.provide('gpub.diagrams.svg');
 
--- src/diagrams/smartgo/smartgo_renderer.js
+++ src/diagrams/smartgo/smartgo_renderer.js
@@ -3,22 +3,25 @@ goog.provide('gpub.diagrams.smartgo.Renderer');
 /**
  * Spec: http://www.smartgo.com/pdf/gobookformat.pdf
  *
- *
  * Some notes:
  * -   All books must start with something like:
  *     ::book(#mahbook) title="Example Title" author="Somebody"
  * -   Each file contains one book -- the file extension should be .gobook.
  *
- * Note: smart go diagrams are indexed from the bottom left:
- * 19
- * 18
- * ..
- * 3
- * 2
- * 1
- *   A B C D E F G H J K ...
  *
- * As with Igo, I is skipped
+ * More details on figure/diagram types: from the doc:
+ *
+ * fig: Main figures of a game, based on a Go game given as Go data, with move
+ * numbers reflecting the moves in the game, and the move numbers listed below
+ * the figure. Shown at full size.
+ *
+ * dia: Diagrams showing alternative move sequences. Move numbering starts at
+ * 1.  Usually shown at a slightly smaller scale than the figures. (This
+ * default setting can be changed with the fullWidth attribute.)
+ *
+ * prb: Diagrams showing a problem diagram. Problems are shown at full width,
+ * and move input will react differently, giving feedback on correct or wrong
+ * solutions.
  *
  * @constructor @final @struct
  */
@@ -35,9 +38,8 @@ gpub.diagrams.smartgo.Renderer.prototype = {
   render: function(flat, opt) {
     return '';
   },
-
   /**
-   * Render-inline the
+   * Render-inline some inline text with smartgo
    * @param {string} text
    * @param {!gpub.api.DiagramOptions} opt
    * @return {string} The processed text
@@ -46,3 +48,4 @@ gpub.diagrams.smartgo.Renderer.prototype = {
     return '';
   }
 };
+
