0d2098aa35b9bbda6997fb7b4529b5c8de0c3a6a
Josh Hoak <jrhoak@gmail.com>
2017-03-11 21:14:16 -0800

    Some sketching of templates.

--- gpub-node/gpub-concat.js
+++ gpub-node/gpub-concat.js
@@ -15436,3 +15436,45 @@ gpub.book.latex.pdfx = {
  * }}
  */
 gpub.book.latex.PdfxOptions;
+
+goog.provide('gpub.templates');
+goog.provide('gpub.templates.Style');
+goog.provide('gpub.templates.Templater');
+
+
+gpub.templates = {};
+
+
+/**
+ * Built-in Styles.
+ *
+ * @enum {string}
+ */
+gpub.templates.Style = {
+  /**
+   * A Commentary-style template used to create the Relentless.
+   */
+  RELENTLESS_COMMENTARY_LATEX: 'RELENTLESS_COMMENTARY_LATEX',
+
+  /**
+   * A template designed to make problem PDFs.
+   */
+  PROBLEM_LATEX: 'PROBLEM_LATEX',
+
+  /**
+   * A template designed to make problem Ebooks.
+   */
+  PROBLEM_EBOOK: 'PROBLEM_EBOOK',
+};
+
+
+/**
+ * @private {!Object<gpub.templates.Style, gpub.templates.Templater>}
+ */
+gpub.templates.registry_ = {};
+
+
+/**
+ * @typedef {function()}
+ */
+gpub.templates.Templater;
--- src/templates/templates.js
+++ src/templates/templates.js
@@ -1,3 +1,41 @@
 goog.provide('gpub.templates');
+goog.provide('gpub.templates.Style');
+goog.provide('gpub.templates.Templater');
+
 
 gpub.templates = {};
+
+
+/**
+ * Built-in Styles.
+ *
+ * @enum {string}
+ */
+gpub.templates.Style = {
+  /**
+   * A Commentary-style template used to create the Relentless.
+   */
+  RELENTLESS_COMMENTARY_LATEX: 'RELENTLESS_COMMENTARY_LATEX',
+
+  /**
+   * A template designed to make problem PDFs.
+   */
+  PROBLEM_LATEX: 'PROBLEM_LATEX',
+
+  /**
+   * A template designed to make problem Ebooks.
+   */
+  PROBLEM_EBOOK: 'PROBLEM_EBOOK',
+};
+
+
+/**
+ * @private {!Object<gpub.templates.Style, gpub.templates.Templater>}
+ */
+gpub.templates.registry_ = {};
+
+
+/**
+ * @typedef {function()}
+ */
+gpub.templates.Templater;
