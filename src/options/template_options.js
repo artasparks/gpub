6e74affe1a70a3c0f98fd645f881265e08df5d79
Josh Hoak <jrhoak@gmail.com>
2017-05-16 20:36:17 -0700

    Add an AZW3 option.

--- src/options/template_options.js
+++ src/options/template_options.js
@@ -90,6 +90,13 @@ gpub.opts.TemplateOptions = function(opt_options) {
    * @const {string|undefined}
    */
   this.colorProfileFilePath = o.colorProfileFilePath || undefined;
+
+  /**
+   * Whether or not to format EBooks for Kindle (AZW3/KF8). Kindle is much more
+   * restrictive than EPub3, so it's often the case that the Kindle format
+   * needs to be different than the standard EPub3 formatting.
+   */
+  this.azw3Format = o.azw3Format || false;
 };
 
 
