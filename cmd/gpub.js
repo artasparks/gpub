94e027f48a8cb8a2f1b0daeae1d95da5ef3545c6
Josh Hoak <jrhoak@gmail.com>
2017-10-24 20:40:36 -0700

    Playing around with new command line utility for gpub.

---
+++ cmd/gpub.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+// var gpub = require('gpub');
+
+program
+  .version('0.1.0');
+  // Global options
+
+program
+  .command('init')
+  .description('initializes a book directory')
+  .action(function(env, options){
+    console.log('zog');
+  });
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
